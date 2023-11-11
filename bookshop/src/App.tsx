027109ffc4cabcd6c8d44855bf815a4c30b85dff
@dromomania <ledavet.ta@gmail.com>
2023-11-12 00:53:30 +0300

    feat: added navbar

--- bookshop/src/App.tsx
+++ bookshop/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import './App.scss';
+import { NavigationBar } from './components/NavigationBar';
 
 
 function App() {
   return (
     <div className="App">
-      Hello
+    <NavigationBar />
     </div>
   );
 }
---
+++ bookshop/src/components/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,29 @@
+import { Navbar, Container, Nav, InputGroup, Form, Button } from 'react-bootstrap';
+
+const NavigationBar = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">B-World</Navbar.Brand>
+        <Nav className="gap-2">
+          <InputGroup >
+            <Form.Control
+              placeholder="Type any book here"
+              aria-label="Type any book here"
+            />
+            <Button variant="outline-secondary" id="button-addon2">
+            <i className="bi bi-search"></i>
+            </Button>
+          </InputGroup>
+          <Nav.Link href="#cart"><i className="bi bi-cart4"></i></Nav.Link>
+          <div className="vr"></div>
+          <Nav.Link href="#liked"><i className="bi bi-heart-fill"></i></Nav.Link>
+          <div className="vr"></div>
+          <Nav.Link href="#myprofile"><i className="bi bi-person-fill"></i></Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export { NavigationBar }
